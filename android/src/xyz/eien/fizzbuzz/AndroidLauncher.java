37a711768aea0c1de1d87da8226f58ef91e89085
kuzai <walkerhwood@gmail.com>
2016-02-20 20:48:16 -0500

    Project Creation
    yay

---
+++ android/src/xyz/eien/fizzbuzz/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package xyz.eien.fizzbuzz;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import xyz.eien.fizzbuzz.FizzBuzzGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FizzBuzzGame(), config);
+	}
+}
